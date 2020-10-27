15c3fc6223ff5006c3eb20bb0a247f00fc0a786d
Robert Osborn <robdosborn@gmail.com>
2020-10-26 20:58:48 -0500

    Preload large images

--- js/gallery.js
+++ js/gallery.js
@@ -4,23 +4,31 @@
 function activateGallery() {
   let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
   let mainImage = document.querySelector("#gallery-photo img");
-  // Image info to be updated
-  let galleryInfo = document.querySelector("#gallery-info");
-  let title       = galleryInfo.querySelector(".title");
-  let description = galleryInfo.querySelector(".description");
-
+  
   thumbnails.forEach(function(thumbnail) {
+    
+    // Preload large images.
+    let newImageSrc  = thumbnail.dataset.largeVersion;
+    let largeVersion = new Image();
+    largeVersion.src = newImageSrc;
+
     thumbnail.addEventListener("click", function() {
-      // Set clicked image as main image.
-      let newImageSrc = thumbnail.dataset.largeVersion;
+
+      // Set clicked image as display image.
       mainImage.setAttribute("src", newImageSrc);
-      mainImage.setAttribute("alt", thumbnail.dataset.description);
+      mainImage.setAttribute("alt", thumbnail.alt);
 
       // Change which image is current.
-      document.querySelector(".current").classList.remove("current");
-      thumbnail.parentNode.classList.add("current");
+      let currentClass = "current";
+
+      document.querySelector(".current").classList.remove(currentClass);
+      thumbnail.parentNode.classList.add(currentClass);
 
       // Update image info.
+      let galleryInfo = document.querySelector("#gallery-info");
+      let title       = galleryInfo.querySelector(".title");
+      let description = galleryInfo.querySelector(".description");
+
       title.innerHTML       = thumbnail.dataset.title;
       description.innerHTML = thumbnail.dataset.description;
     });
